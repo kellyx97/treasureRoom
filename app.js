34b114055bbb54489868417c218c97a378704f50
Humad Shah <humad@live.com>
2017-11-21 19:13:18 -0500

    database additions

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var express = require("express");
-var app = express();
-var path = require("path");
-var bodyParser = require("body-parser");
-var port = process.env.PORT || 3000;
+const express = require("express");
+const app = express();
+const path = require("path");
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, 'public'))); // location of scripts and styles
@@ -13,6 +13,22 @@ app.get("/", function(req, res){
     res.render("index");
 });
 
+app.get("/", function(req, res) {
+
+});
+
 app.listen(port, function(){
     console.log("Listening on port " + port);
 });
+
+// Database
+const mongoose = require('mongoose');
+var testUser = "test";
+var testPass = "test";
+
+var thoughtSchema = mongoose.Schema({
+    text: {type: String, required: true},
+    dateTime: {type: String, required: true}
+});
+
+mongoose.model('Thought', thoughtSchema, 'thoughts');
